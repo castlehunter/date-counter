f808114e333bcb647a1cb2ea56cd897772b80bef
castlehunter <vvxiaobai@gmail.com>
2024-06-06 22:49:08 -0400

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  // const [day, setDay] = useState(0);
+
+  function getDate(num) {
+    const date = new Date();
+    date.setDate(date.getDate() + num);
+    return date.toLocaleDateString("en-US");
+  }
+
+  function handleStepMinus() {
+    setStep((s) => s - 1);
+  }
+
+  function handleStepPlus() {
+    setStep((s) => s + 1);
+  }
+
+  function handleCountMinus() {
+    setCount((c) => c + step);
+  }
+
+  function handleCountPlus() {
+    setCount((c) => c + step);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div>
+        <button onClick={handleStepMinus}>-</button>Step: {step}
+        <button onClick={handleStepPlus}>+</button>
+      </div>
+      <div>
+        <button onClick={handleCountMinus}>-</button>Count: {count}
+        <button onClick={handleCountPlus}>+</button>
+      </div>
+
+      {count === 0 && <span>Today is</span>}
+      {count > 0 && <span>{count} day from today is</span>}
+      {count < 0 && <span>{count} day ago is</span>}
     </div>
   );
 }
